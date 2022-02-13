1a2b4b1769ce3cf0b57a385b1c231c2b96dd2dec
Ripal Nayak <ripal.nayak@kemsys.com>
2022-02-13 16:59:36 -0500

    Add project files.

---
+++ GuardianTD/Controllers/UserController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using GuardianTD.Models;
+using System.Net;
+using System.Net.Http;
+
+namespace GuardianTD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.users";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetById(int id)
+        {
+            string query = @"select * from dbo.users where user_id=@id";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id",id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(User user)
+        {
+            string query = @"
+                            insert into dbo.users
+                            ([first_name],[last_name],[created_at],[email],[password],[active])
+                            values (@FirstName,@LastName,@CreatedAt,@Email,@Password,@Active)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    myCommand.Parameters.AddWithValue("@LastName", user.LastName);
+                    myCommand.Parameters.AddWithValue("@CreatedAt", DateTime.UtcNow);
+                    myCommand.Parameters.AddWithValue("@Email", user.Email);
+                    myCommand.Parameters.AddWithValue("@Password", user.Password);
+                    myCommand.Parameters.AddWithValue("@Active", 1);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("User Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(User user)
+        {
+            string query = @"
+                            update dbo.users
+                            set first_name= @FirstName,last_name=@LastName,email=@Email,password=@Password where user_id=@Id";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", user.Id);
+                    myCommand.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    myCommand.Parameters.AddWithValue("@LastName", user.LastName);
+                    myCommand.Parameters.AddWithValue("@Email", user.Email);
+                    myCommand.Parameters.AddWithValue("@Password", user.Password);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("User Details Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.users
+                             where user_id=@Id";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id",id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("User Deleted Successfully");
+        }
+
+        [HttpGet("Authenticate")]
+        public ActionResult Authenticate(string emailId,string password)
+        {
+            bool hasResult = false;
+            string query = @"select * from dbo.users where email=@EmailId and password=@Password";
+            string sqlDataSource = _configuration.GetConnectionString("GuardianTDConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Emailid", emailId);
+                    myCommand.Parameters.AddWithValue("@Password", password);
+                    myReader = myCommand.ExecuteReader();
+                    if(myReader.HasRows)
+                    {
+                        hasResult = true;
+                    }
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            if(hasResult)
+                return new OkResult();
+            else
+                return BadRequest("Invalid UserId or Password.Please try again.");
+
+        }
+    }
+}
